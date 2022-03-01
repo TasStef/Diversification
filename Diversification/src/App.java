04393909a895fde2b5f5e4bc8959a7621be13350
TasStef <9004757@gmai.com>
2022-03-01 14:03:04 +0000

    Working for 1 stocker

--- Diversification/src/App.java
+++ Diversification/src/App.java
@@ -2,8 +2,24 @@
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Stocker stocker = new Stocker(1); 
+		
 
+	
+				
+		
+		
+		System.out.println(stocker.getStockerID());
+		System.out.println(stocker.getWallet());
+		stocker.setNumberOfStocks();
+		System.out.println(stocker.getNumberOfStocks());
+		
+		for (int i = 0; i < 10; i++) {
+			stocker.stockResult();
+			System.out.println("year " + i +"\nwallet " +stocker.getWallet() + "||Stocks " +stocker.getNumberOfStocks());
+		}
+		
+		
 	}
-
 }
